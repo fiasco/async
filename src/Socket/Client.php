f453c96fab4ba98668d97abf6d4d1389fc6f2f23
Josh Waihi <josh.waihi@acquia.com>
2022-04-01 13:11:28 +1300

    Add waits to clients incase the server hasn't started listening when attempting to send messages.

--- src/Socket/Client.php
+++ src/Socket/Client.php
@@ -4,8 +4,13 @@ namespace Async\Socket;
 
 use Async\Exception\SocketClientException;
 use Async\Message;
+use Psr\Log\LoggerAwareTrait;
 
 class Client {
+
+  const MAX_RETRIES = 600;
+
+  use LoggerAwareTrait;
 
   protected $socket;
 
@@ -23,7 +28,20 @@ class Client {
         throw new SocketClientException("socket_create() failed: reason: " . socket_strerror(socket_last_error()));
     }
 
-    $result = socket_connect($socket, $this->address, $this->port);
+    $retries = 0;
+    while(!$result = @socket_connect($socket, $this->address, $this->port)) {
+      if (isset($this->logger)) {
+        $this->logger->info("Socket server not up yet: {error}. Sleeping...", [
+          'error' => socket_strerror(socket_last_error($socket)),
+        ]);
+      }
+      sleep(1);
+      $retries++;
+
+      if ($retries > self::MAX_RETRIES) {
+        break;
+      }
+    }
 
     if ($result === false) {
         throw new SocketClientException("socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)));
