b1406666fba26fe5fa2a2ea1bf8a293a6762bebd
Josh Waihi <josh.waihi@acquia.com>
2022-04-03 08:21:39 +1200

    Improved looping errors example.

--- examples/looping-errors.php
+++ examples/looping-errors.php
@@ -1,18 +1,34 @@
 <?php
 use Async\ForkManager;
 use Async\ForkInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
 $forkManager = new ForkManager();
-// Wait up to 3 seconds for fork response before giving up.
-$forkManager->setWaitTimeout(3);
+$forkManager->setWaitTimeout(2);
+$forkManager->setLogger(new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_NORMAL)));
 
-$forkManager->create()->run(fn() => 'foo');
-// This will not show unless true is passed as ->getForkResults(true).
-// This will cause a fatal error in the child that will not report back
-// and will therefore rely on the forkManager timeout to error out.
-$forkManager->create()->run(fn() => new \Exception('bar'));
+// This will result in a caught fatal error from the child.
+$forkManager->create()->run(fn() => @$foo->doNothing());
+
+// This fork will catch a thrown exception and trigger the onError callback
+// in the parent thread.
+$forkManager->create()->run(function (ForkInterface $fork) {
+  // You can set labels inside the fork to give better information about
+  // what the specific fork is doing.
+  $fork->setLabel("Fork that throws an error.");
+  throw new \Exception('bar');
+});
+
+// This will result in a caught fatal error from the child.
+$forkManager->create()->run(function (ForkInterface $fork) {
+  $fork->setLabel("Fork that will timeout.");
+  sleep(4);
+});
+
+// This fork will not error.
 $forkManager->create()->run(fn() => 'baz');
 
 echo "Waiting for forks to complete...\n";
@@ -23,5 +39,11 @@ $forkManager->awaitForks();
 foreach ($forkManager->getForks(ForkInterface::STATUS_ERROR) as $fork) {
   // Result will be instance Async\Exception\ChildExceptionDetected.
   echo sprintf("Fork '%s' encountered an error:\n", $fork->getLabel());
-  echo $fork->getResult()->getMessage() . PHP_EOL;
+  // echo $fork->getResult()->getMessage() . PHP_EOL;
+}
+
+foreach ($forkManager->getForks(ForkInterface::STATUS_COMPLETE) as $fork) {
+  // Result will be instance Async\Exception\ChildExceptionDetected.
+  echo sprintf("Fork '%s' completed.\n", $fork->getLabel());
+  // echo $fork->getResult()->getMessage() . PHP_EOL;
 }
